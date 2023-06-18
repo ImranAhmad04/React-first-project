00a3823ec9ec1d19cc957dd5e18eeaeffd856c41
ImranAhmad04 <emranmd368@gmail.com>
2023-06-18 12:40:12 +0600

    its not latest update

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,13 @@ function App() {
   return (
     <>
     <h2 style={{"text-align": "center"}}> Result</h2>
-    <table>
-      <tr> <th>Roll</th> <th>Name</th> <th>Madrasah </th> <th>Result</th></tr></table>
-      {Data.map((items , index) =><Copy key={index} Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah}  result={items.result}/> )}
+    
+      <table>
+      <tr> <th>Roll</th> <th>Name</th> <th>Madrasah </th> <th>Result</th></tr>
+      </table>
+       {Data.map((items ,  index) =><Copy key={index} Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah}  result={items.result}/> )}
+       {Data.map((items, index) =><Copy key={index} Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah} result={items.result}/> )}
        
-       {/* {Data.map((items ,  index) =><Copy key={index} Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah}  result={items.result}/> )}
-       {Data.map((items, index) =><Copy key={index} Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah} result={items.result}/> )} */}
       {/* <Copy Name = {Data[1].name} Roll={Data[1].Roll} Madrasah={Data[1].Madrasah}/> 
       <Copy Name = {Data[2].name} Roll={Data[2].Roll} Madrasah={Data[1].Madrasah}/> */}
       
