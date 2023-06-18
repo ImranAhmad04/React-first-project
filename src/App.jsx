7f02dc8461874f4bb7c24f52e83482f2ad815bba
ImranAhmad04 <emranmd368@gmail.com>
2023-06-18 12:52:15 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,22 @@ import Copy from './Components/Copy'
 import Data from "./Components/List.json"
 function App() {
   const [count, setCount] = useState(0)
-//  let Items = []
-//  Items = Data.map((items) =><Copy Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah}/> )
-
-
-//  for( let x = 0 ; x < Data.length ; x++){
-//   Items.push(<Copy Name = {Data[x].name} Roll={Data[x].Roll} Madrasah={Data[x].Madrasah}/>)
-//  }
+const users = [
+  {
+    FullName:"imran ahmed",
+    age:19,
+    phone:[
+      
+       { home:"017......"},
+       { office:"016...."}
+       
+      
+    ]
+  } 
+]
   return (
     <>
-    <h2 style={{"text-align": "center"}}> Result</h2>
-    
-      <table>
-      <tr> <th>Roll</th> <th>Name</th> <th>Madrasah </th> <th>Result</th></tr>
-      </table>
-       {Data.map((items ,  index) =><Copy key={index} Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah}  result={items.result}/> )}
-       {Data.map((items, index) =><Copy key={index} Name = {items.name} Roll={items.Roll} Madrasah={items.Madrasah} result={items.result}/> )}
-       
-      {/* <Copy Name = {Data[1].name} Roll={Data[1].Roll} Madrasah={Data[1].Madrasah}/> 
-      <Copy Name = {Data[2].name} Roll={Data[2].Roll} Madrasah={Data[1].Madrasah}/> */}
-      
+     <h1>Nested Lists</h1>
     </>
   )
 }
--- src/Components/Copy.jsx
+++ src/Components/Copy.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Data from "./List.json"
-import './copy.css'
+
 export default function Copy(props) {
   return (
-    <div id='result-table'> 
-      
-       <table>
-       {/* <tr> <th>Roll</th> <th>Name</th> <th>Madrasah </th> <th>Result</th></tr> */}
-       <tr><td>{props.Roll}</td> <td>{props.Name}</td> <td>{props.Madrasah}</td> <td>{props.result}</td></tr>
-       </table>
-       
+    <div> 
         
     </div>
   )
